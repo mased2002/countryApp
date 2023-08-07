88788a0503d5e87368f7dcc7f76caa7f468a5dde
mased2002 <edsam2302@gmail.com>
2023-08-07 20:41:23 +0200

    works well only some icons left

---
+++ countries.js
@@ -0,0 +1,121 @@
+const countryName = new URLSearchParams(location.search).get('name')
+console.log(countryName)
+let apiUrl = 'https://restcountries.com/v3.1/';
+let selectedCountry = [];
+let countryContainer = document.querySelector('.country-displayed')
+const flag = document.querySelector('.flag')
+// const flagClas = document.querySelector('.flag-cla')
+const firstDetails = document.querySelector('.first-details')
+const secondDetails = document.querySelector('.second-details')
+const darkbtn = document.querySelector('.dark')
+const wholeBody = document.querySelector('.body')
+const first = document.querySelector('.first')
+const second = document.querySelector('.second')
+const backBtn = document.querySelector('.back-btn')
+
+darkbtn.addEventListener("click", ()=>{
+  wholeBody.classList.add('darkened')
+  first.classList.add('darkened')
+  second.classList.add('darkened')
+  darkbtn.classList.add('darkenedBtn')
+  backBtn.classList.add('darkenedBtn')
+
+ })
+
+getDetails()
+.then(() => {
+    // delete_this();
+    display_countries();
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+
+function getDetails(){
+    return new Promise((resolve, reject) => {
+        fetch(apiUrl+`name/${countryName}`)
+        .then(result => result.json())
+        .then(real => {
+            selectedCountry = real;
+            console.log(selectedCountry);
+            resolve()
+        })
+        .catch((error)=>{
+            console.log("failed to reach process", error)
+            reject()
+        })
+    })
+}
+function addLabelAndData(container, labelText, dataText, elementTag = 'span') {
+    const labelElement = document.createElement('strong');
+    labelElement.textContent = labelText;
+  
+    const dataElement = document.createElement(elementTag);
+    dataElement.textContent = dataText;
+  
+    container.appendChild(labelElement);
+    container.appendChild(dataElement);
+  }
+
+  function display_countries(){
+
+    const flagOf = document.createElement("img")
+    flagOf.src = selectedCountry[0].flags.png
+    flagOf.style.width= '100%'
+    flag.appendChild(flagOf)
+
+
+    const nameOf = document.querySelector('.name')
+    nameOf.textContent = selectedCountry[0].name.common
+
+    const nativeNameOf = document.querySelector('.native-name')
+    addLabelAndData(nativeNameOf, 'Native Name: ', `${Object.values(selectedCountry[0].name.nativeName)[0].official}`)
+
+    const populationOf = document.querySelector('.population')
+    addLabelAndData(populationOf, 'Population: ',selectedCountry[0].population )
+
+    const regionOf = document.querySelector('.region')
+    addLabelAndData(regionOf, 'Region: ', selectedCountry[0].region)
+
+    const subRegionOf = document.querySelector('.sub-region')
+    addLabelAndData(subRegionOf, 'Sub Region: ', selectedCountry[0].subregion)
+
+    const capital = document.querySelector('.capital')
+    addLabelAndData(capital, 'Capital: ', selectedCountry[0].capital[0])
+
+    
+    // nativeNameOf.textContent = selectedCountry[0].name.official
+    // populationOf.textContent = selectedCountry[0].population
+    // regionOf.textContent = selectedCountry[0].region
+    // capital.textContent = selectedCountry[0].capital[0]
+    // subRegionOf.textContent = selectedCountry[0].subregion
+    firstDetails.appendChild(nameOf)
+    firstDetails.appendChild(nativeNameOf)
+    firstDetails.appendChild(populationOf)
+    firstDetails.appendChild(regionOf)
+    firstDetails.appendChild(subRegionOf)
+    firstDetails.appendChild(capital)
+
+    const topLevel = document.querySelector('.top-level')
+    addLabelAndData(topLevel, 'Top Level Domain: ', selectedCountry[0].tld[0])
+    const currency = document.querySelector('.Currencies')
+    addLabelAndData(currency, 'Currency: ', `${Object.values(selectedCountry[0].currencies)[0].name} (${Object.values(selectedCountry[0].currencies)[0].symbol})`)
+    const language = document.querySelector('.languages')
+    let languagesArr = getLang()
+    addLabelAndData(language, 'Language: ', languagesArr.join(','))
+// topLevel.textContent = selectedCountry[0].tld[0]
+// currency.textContent =  `${Object.values(selectedCountry[0].currencies)[0].name} (${Object.values(selectedCountry[0].currencies)[0].symbol})`
+// language.textContent = selectedCountry[0].languages.eng
+    secondDetails.appendChild(topLevel)
+    secondDetails.appendChild(currency)
+    secondDetails.appendChild(language)
+  }
+  
+  function getLang(){
+    let arr = []
+    for(let i in selectedCountry[0].languages){
+      arr.push(selectedCountry[0].languages[i])
+    }
+    return arr
+  }
--- script.js
+++ script.js
@@ -1,119 +1,170 @@
 
 
-  let apiUrl = 'https://restcountries.com/v3.1/';
-  let selectedRegion = "";
+let apiUrl = 'https://restcountries.com/v3.1/';
+let selectedRegion = "";
 let searchedCountry = "";
-  let names = [];
-  let countriesData = [];
-  const countryContainer = document.querySelector('.display-cont');
-  let filter = document.querySelector('#regions');
+let names = [];
+let countriesData = [];
+const countryContainer = document.querySelector('.display-cont');
+let filter = document.querySelector('#regions');
 let search = document.querySelector('.search-cont input')
-search.addEventListener('change', ()=>{
-searchedCountry = search.value;
-  console.log(searchedCountry)
-  searchCountry()
-  .then(() => {
-      delete_this();
-      display_countries();
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+const darkbtn = document.querySelector('.dark')
+const first = document.querySelector('.first')
+const second = document.querySelector('.second')
+const wholeBody = document.querySelector('.body')
+
+darkbtn.addEventListener("click", ()=>{
+ darkbtn.classList.add('darkenedBtn')
+  first.classList.add('darkened')
+ wholeBody.classList.add('darkened')
+ countryContainer.classList.add('darkened')
 })
 
-  filter.addEventListener('change', async () => {
-    selectedRegion = filter.value;
-    console.log(selectedRegion);
+search.addEventListener('keyup', ()=>{
+searchedCountry = search.value;
+console.log(searchedCountry)
+searchCountry()
+.then(() => {
+    delete_this();
+    display_countries();
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+})
 
-    try {
-      await filterByRegion();
-      delete_this();
-      display_countries();
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+filter.addEventListener('change', async () => {
+  selectedRegion = filter.value;
+  console.log(selectedRegion);
+
+  try {
+    await filterByRegion();
+    delete_this();
+    display_countries();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+
+
+home_page()
+  .then(() => {
+    delete_this();
+    display_countries();
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
   });
 
- 
-home_page()
-    .then(() => {
-      delete_this();
-      display_countries();
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+  
 
 // this is the function used when filtering by regions
-  async function filterByRegion() {
-    let result = await fetch(apiUrl + `region/${selectedRegion}`);
-    let real = await result.json();
-    console.log(real);
-    countriesData = real;
+async function filterByRegion() {
+  let result = await fetch(apiUrl + `region/${selectedRegion}`);
+  let real = await result.json();
+  console.log(real);
+  countriesData = real;
 
-  }
+}
 
-  function display_countries() {
-    // display the countries and their components.
-    countriesData.forEach(el => {
-    const countryElement = document.createElement('div')
-    const flagOf = document.createElement('img')
-    const nameOf = document.createElement('h2')
-    const capitalCity = document.createElement('h3')
-    const currencyOf = document.createElement('p')
-    const populationOf = document.createElement('p')
-                         // countryElement.classList.add('size');                    // countryElement.style.width = '150px'
-    flagOf.src = el.flags.png
-    nameOf.textContent = el.name.common
-    capitalCity.textContent = `Capital:${el.capital[0]}`
-    currencyOf.textContent = `Currency: ${Object.values(el.currencies)[0].name} (${Object.values(el.currencies)[0].symbol})`;
-      populationOf.textContent = `Population: ${el.population}`
-      
-      countryContainer.appendChild(countryElement)
-      countryElement.appendChild(flagOf)
-      countryElement.appendChild(nameOf)
-      countryElement.appendChild(capitalCity)
-      countryElement.appendChild(populationOf)
-      countryElement.appendChild(currencyOf)
-    })
-  }
+function addLabelAndData(container, labelText, dataText, elementTag = 'span') {
+  const labelElement = document.createElement('strong');
+  labelElement.textContent = labelText;
 
-  function delete_this() {
-    // Remove all children from the countryContainer div
-    while (countryContainer.firstChild) {
-      countryContainer.removeChild(countryContainer.firstChild);
-    }
-  }
+  const dataElement = document.createElement(elementTag);
+  dataElement.textContent = dataText;
 
-  function home_page() {
-    return new Promise((resolve, reject) => {
-      fetch(apiUrl + "all")
-        .then(result => result.json())
-        .then(real => {
-          console.log(real);
-        countriesData = real
-        resolve(); 
-          })
-        .catch(error => {
-          reject(error);
-        })
-          // Resolve the promise once data is fetched and processed
-        });
-  }
+  container.appendChild(labelElement);
+  container.appendChild(dataElement);
+}
 
-  function searchCountry(){
-    
-    return new Promise((resolve, reject) => {
-      fetch(apiUrl + `name/${searchedCountry}`)
-      .then(result => result.json())
-         .then(real => {
-           console.log(real);
-         countriesData = real
-         resolve(); 
-           })
-         .catch(error => {
-           reject(error);
-         });
-    });
+function display_countries() {
+
+
+  countriesData.forEach(el => {
+    const countryLink = document.createElement('a');
+    countryLink.setAttribute('href', `country.html?name=${encodeURIComponent(el.name.common)}`);
+    // countryLink.setAttribute('target', '_blank');
+    countryLink.classList.add('links');
   
+    const countryElement = document.createElement('div');
+    countryElement.style.textAlign = 'left';
+    countryElement.classList.add('country')
+  
+    var contr = document.createElement('div');
+    contr.classList.add('yrount')
+
+    const flagOf = document.createElement('img');
+    flagOf.style.height = '150px';
+    flagOf.style.width = '250px';
+    flagOf.src = el.flags.png;
+  
+    const nameOf = document.createElement('h2');
+    nameOf.style.fontSize = '20px'
+    nameOf.textContent = el.name.common;
+  
+    const capitalCity = document.createElement('p');
+    addLabelAndData(capitalCity, 'Capital:', el.capital[0]);
+  
+    const currencyOf = document.createElement('p');
+    addLabelAndData(currencyOf, 'Currency:', `${Object.values(el.currencies)[0].name} (${Object.values(el.currencies)[0].symbol})`);
+  
+    const populationOf = document.createElement('p');
+    addLabelAndData(populationOf, 'Population:', `${el.population}`);
+  
+    countryContainer.appendChild(countryLink);
+    countryLink.appendChild(countryElement);
+    countryElement.appendChild(flagOf);
+    countryElement.appendChild(nameOf);
+    countryElement.appendChild(capitalCity);
+    countryElement.appendChild(currencyOf);
+    countryElement.appendChild(populationOf);
+    countryElement.appendChild(contr)
+    contr.appendChild(nameOf)
+    contr.appendChild(capitalCity)
+    contr.appendChild(currencyOf)
+    contr.appendChild(populationOf)
+  });
+}
+
+function delete_this() {
+  // Remove all children from the countryContainer div
+  while (countryContainer.firstChild) {
+    countryContainer.removeChild(countryContainer.firstChild);
   }
+}
+
+function home_page() {
+  return new Promise((resolve, reject) => {
+    fetch(apiUrl + "all")
+      .then(result => result.json())
+      .then(real => {
+        console.log(real);
+      countriesData = real
+      resolve(); 
+        })
+      .catch(error => {
+        reject(error);
+      })
+        // Resolve the promise once data is fetched and processed
+      });
+}
+
+function searchCountry(){
+  
+  return new Promise((resolve, reject) => {
+    fetch(apiUrl + `name/${searchedCountry}`)
+    .then(result => result.json())
+       .then(real => {
+         console.log(real);
+       countriesData = real
+       resolve(); 
+         })
+       .catch(error => {
+         reject(error);
+       });
+  });
+
+}
+
+
